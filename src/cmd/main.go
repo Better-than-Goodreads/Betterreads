bd82a130749d1f1f794eea454986e13a0337349b
Theo Lijs <tlijs@fi.uba.ar>
2024-10-21 18:19:15 -0300

    Updated doc path

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -9,7 +9,7 @@ import (
 // @version 1.0
 // @description This is a  server for Swagger with Gin.
 // @host localhost:8080
-// @BasePath /api/v1
+// @BasePath /
 
 func main() {
 	r := application.NewRouter(":8080")
--- src/docs/docs.go
+++ src/docs/docs.go
@@ -410,7 +410,7 @@ const docTemplate = `{
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:8080",
-	BasePath:         "/api/v1",
+	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "BetterReads API",
 	Description:      "This is a  server for Swagger with Gin.",
