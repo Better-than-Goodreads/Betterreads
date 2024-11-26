a7cc5bfd5a7770777a147e607a0cc87a00a21c35
Theo Lijs <tlijs@fi.uba.ar>
2024-11-26 17:16:30 -0300

    Fix type to status in bookshelf request

--- src/internal/domains/bookshelf/controller/bookshelf.go
+++ src/internal/domains/bookshelf/controller/bookshelf.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"net/http"
 
+	bookService "github.com/betterreads/internal/domains/books/service"
 	"github.com/betterreads/internal/domains/bookshelf/models"
 	"github.com/betterreads/internal/domains/bookshelf/service"
-	bookService "github.com/betterreads/internal/domains/books/service"
 	aux "github.com/betterreads/internal/pkg/controller"
 	er "github.com/betterreads/internal/pkg/errors"
 	"github.com/gin-gonic/gin"
@@ -42,9 +42,9 @@ func (bc *BookshelfController) GetBookShelf(c *gin.Context) {
 		c.AbortWithError(errDetails.Status, errDetails)
 		return
 	}
-	shelfType := c.Query("type")
+	shelfType := c.Query("status")
 	if shelfType == "" {
-		errParam := er.ErrorParam{Name: "type", Reason: "status is required"}
+		errParam := er.ErrorParam{Name: "status", Reason: "status is required"}
 		errDetails := er.NewErrorDetailsWithParams("Error when getting shelf", http.StatusBadRequest, errParam)
 		c.AbortWithError(errDetails.Status, errDetails)
 		return
@@ -68,7 +68,6 @@ func (bc *BookshelfController) GetBookShelf(c *gin.Context) {
 	c.JSON(200, shelf)
 }
 
-
 // SearchBookShelf godoc
 // @Summary Search books in shelf of an user
 // @Description Search books in shelf of an user. The search can be filtered by genre, sorted by avg_ratings, total_ratings and date. The direction can be asc or desc.
@@ -85,25 +84,25 @@ func (bc *BookshelfController) GetBookShelf(c *gin.Context) {
 // @Router /users/{id}/shelf/search [get]
 func (bc *BookshelfController) SearchBookShelf(c *gin.Context) {
 
-    userId, err := uuid.Parse(c.Param("id"))
+	userId, err := uuid.Parse(c.Param("id"))
 	if err != nil {
 		errDetails := er.NewErrorDetails("Error when getting shelf", fmt.Errorf("invalid user id"), http.StatusBadRequest)
 		c.AbortWithError(errDetails.Status, errDetails)
 		return
 	}
 
-    shelfType := c.Query("type")
-    if shelfType == "" {
-        errParam := er.ErrorParam{Name: "type", Reason: "status is required"}
-        errDetails := er.NewErrorDetailsWithParams("Error when getting shelf", http.StatusBadRequest, errParam)
-        c.AbortWithError(errDetails.Status, errDetails)
-        return
-    }
+	shelfType := c.Query("status")
+	if shelfType == "" {
+		errParam := er.ErrorParam{Name: "status", Reason: "status is required"}
+		errDetails := er.NewErrorDetailsWithParams("Error when getting shelf", http.StatusBadRequest, errParam)
+		c.AbortWithError(errDetails.Status, errDetails)
+		return
+	}
 
-    genre := c.Query("genre")
+	genre := c.Query("genre")
 	sort := c.Query("sort")
 	direction := c.Query("direction")
-    books, err := bc.service.SearchBookShelf(userId, shelfType, genre, sort, direction)
+	books, err := bc.service.SearchBookShelf(userId, shelfType, genre, sort, direction)
 	if err != nil {
 		if errors.Is(err, bookService.ErrInvalidSort) {
 			errDetails := er.NewErrorDetailsWithParams(
@@ -119,9 +118,9 @@ func (bc *BookshelfController) SearchBookShelf(c *gin.Context) {
 		} else if errors.Is(err, service.ErrGenreNotFound) {
 			errDetail := er.NewErrorDetails("Error when searching books in shelf", err, http.StatusBadRequest)
 			c.AbortWithError(errDetail.Status, errDetail)
-        } else if errors.Is(err, service.ErrInvalidStatusType) {
-            errDetails := er.NewErrorDetailsWithParams("Error when searching books in shelf",  http.StatusBadRequest, err)
-            c.AbortWithError(errDetails.Status, errDetails)
+		} else if errors.Is(err, service.ErrInvalidStatusType) {
+			errDetails := er.NewErrorDetailsWithParams("Error when searching books in shelf", http.StatusBadRequest, err)
+			c.AbortWithError(errDetails.Status, errDetails)
 		} else {
 			errDetail := er.NewErrorDetails("Error when searching books", err, http.StatusInternalServerError)
 			c.AbortWithError(errDetail.Status, errDetail)
@@ -223,8 +222,6 @@ func (bc *BookshelfController) EditBookInShelf(c *gin.Context) {
 
 }
 
-
-
 // DeleteBookFromShelf godoc
 // @Summary Delete book from shelf
 // @Description Delete book from shelf
@@ -237,32 +234,33 @@ func (bc *BookshelfController) EditBookInShelf(c *gin.Context) {
 // @Failure 404 {object} errors.ErrorDetails
 // @Failure 500 {object} errors.ErrorDetails
 // @Router /users/shelf [delete]
-func (bc *BookshelfController) DeleteBookFromShelf(c * gin.Context) {
+func (bc *BookshelfController) DeleteBookFromShelf(c *gin.Context) {
 	userId, errId := aux.GetLoggedUserId(c)
 	if errId != nil {
 		c.AbortWithError(errId.Status, errId)
 		return
 	}
-    
-    bookId, err := uuid.Parse(c.Query("id"))
-    if err != nil {
-        errDetails := er.NewErrorDetails("Error when deleting book from shelf", fmt.Errorf("invalid book id"), http.StatusBadRequest)
-        c.AbortWithError(errDetails.Status, errDetails)
-        return 
-    }
+
+	bookId, err := uuid.Parse(c.Query("id"))
+	if err != nil {
+		errDetails := er.NewErrorDetails("Error when deleting book from shelf", fmt.Errorf("invalid book id"), http.StatusBadRequest)
+		c.AbortWithError(errDetails.Status, errDetails)
+		return
+	}
 
 	err = bc.service.DeleteBookFromShelf(userId, bookId)
-	if err != nil {if errors.Is(err, service.ErrUserNotFound) {
-		errDetails := er.NewErrorDetails("Error when editing book in shelf", err, http.StatusNotFound)
-		c.AbortWithError(errDetails.Status, errDetails)
-	} else if errors.Is(err, service.ErrBookNotFoundInLibrary) {
-		errDetails := er.NewErrorDetails("Error when editing book in shelf", err, http.StatusBadRequest)
-		c.AbortWithError(errDetails.Status, errDetails)
-	} else {
-		errDetails := er.NewErrorDetails("Error when editing book in shelf", err, http.StatusInternalServerError)
-		c.AbortWithError(errDetails.Status, errDetails)
-	}
-	return
+	if err != nil {
+		if errors.Is(err, service.ErrUserNotFound) {
+			errDetails := er.NewErrorDetails("Error when editing book in shelf", err, http.StatusNotFound)
+			c.AbortWithError(errDetails.Status, errDetails)
+		} else if errors.Is(err, service.ErrBookNotFoundInLibrary) {
+			errDetails := er.NewErrorDetails("Error when editing book in shelf", err, http.StatusBadRequest)
+			c.AbortWithError(errDetails.Status, errDetails)
+		} else {
+			errDetails := er.NewErrorDetails("Error when editing book in shelf", err, http.StatusInternalServerError)
+			c.AbortWithError(errDetails.Status, errDetails)
+		}
+		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Book deleted from shelf"})
 }
